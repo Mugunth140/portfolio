40eb5c804bbc342047e39fe9a5f860239faf9792
Mugunth140 <soulcyber148@gmail.com>
2024-06-06 20:32:38 +0530

    Completed About route with compatability

--- src/components/AboutStack/AboutStack.jsx
+++ src/components/AboutStack/AboutStack.jsx
@@ -1,30 +1,35 @@
 const Aboutstack = () => {
   const AboutStackData = [
     {
-      title: "Frontend",
-      description: "",
+      title: "Clint-Side (User Interface/User Experience)",
+      description:
+        "I specialize in crafting visually appealing, responsive interfaces using React and Next.js. My focus is on delivering intuitive, pixel-perfect designs and smooth animations that enhance user engagement and provide exceptional user experiences.",
     },
     {
-      title: "Backend",
-      description: "",
+      title: "Server-Side (Api's & Database)",
+      description:
+        "I excel in building robust, scalable server-side systems with Node.js, Express, and databases like MongoDB and MySQL. I develop secure, efficient applications and RESTful APIs, ensuring data integrity and writing clean, maintainable code for long-term performance.",
     },
     {
-      title: "Full-Stack",
-      description: "",
+      title: "End-to-End (All-In-One-Package)",
+      description:
+        "As an End-to-End Developer, I integrate my interface and server-side skills to deliver complete web solutions. From UI design with React and Next.js to building server infrastructure, I handle all development aspects. Additionally, I incorporate SEO best practices to ensure your application is discoverable and reaches a wider audience.",
     },
   ];
 
   return (
     <div className="stack-wrapper">
       <div className="stack-title">
-        <h1>I can help you with</h1>
+        <h2>I&apos;m Equipped to Support you with</h2>
       </div>
       <div className="stack-content">
         {AboutStackData.map((data, index) => {
           return (
-            <div className="stack-item" key={index}>
-              <h2>{data.title}</h2>
-              <p>{data.description}</p>
+            <div className="stack-items" key={index}>
+              <h2 className="item-title">{data.title}</h2>
+              <div className="item-description">
+                <p>{data.description}</p>
+              </div>
             </div>
           );
         })}
