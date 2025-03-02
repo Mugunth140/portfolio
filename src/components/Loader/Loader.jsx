da53e4320a66fa291e3ae0cc89eb8f2bc2ea5be6
mugunth140 <mugunth140@gmail.com>
2025-03-02 13:14:26 +0530

    Updated Loader Compoent with improved SEO

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { words } from "./data";
@@ -73,6 +74,29 @@ const Loader = ({ animationComplete }) => {
   }, [animationComplete]);
 
   return (
+    <>
+    <Head>
+    <title>Mugunth | Freelance Developer Portfolio</title>
+    <meta name="description" content="Welcome to Mugunth's portfolio. A passionate freelance developer specialized in JavaScript, Python, and full-stack development. Discover my projects and expertise." />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+
+    <meta name="robots" content="index, follow" />
+    <meta name="keywords" content="Mugunth portfolio, Freelance developer India, JavaScript developer, Python developer, Full-stack developer, React developer, Node.js developer, MongoDB developer, Web development, Portfolio website" />
+    <link rel="canonical" href="https://mugunth.me/" />
+
+    <meta property="og:title" content="Mugunth | Freelance Developer Portfolio" />
+    <meta property="og:description" content="Explore the portfolio of Mugunth, a freelance developer skilled in JavaScript, Python, and full-stack web development." />
+    <meta property="og:image" content="https://mugunth.me/about.jpg" />
+    <meta property="og:url" content="https://mugunth.me/" />
+    <meta property="og:type" content="website" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Mugunth | Portfolio" />
+    <meta name="twitter:description" content="Explore Mugunth's portfolio, a freelance developer skilled in JavaScript, Python, and full-stack development." />
+    <meta name="twitter:image" content="https://mugunth.me/about.jpg" />
+  </Head>
+  
     <div className={styles.loader__wrapper} >
       <div className={styles.loader__progressWrapper} ref={progressWrapperRef}>
         <div className={styles.loader__progress} ref={progressRef}></div>
@@ -95,6 +119,7 @@ const Loader = ({ animationComplete }) => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
